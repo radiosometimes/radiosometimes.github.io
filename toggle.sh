bb45335cf4eff69a540b87e0b52ce765a2a92a73
Pedro V. Moura <pmavfmoura@gmail.com>
2015-06-06 17:01:20 -0400

    made website and script to toggle website status

---
+++ toggle.sh
@@ -0,0 +1,23 @@
+url="http:\/\/45.55.199.85:8000\/sometimes.ogg"
+if [ $2 ]; then
+	url=${2//\//\\/}
+	echo $url
+fi
+
+if [ $1 == "on" ]; then
+	sed -i '12s/$<!--<div/<div/' index.html
+	sed -i '17s/\/div>-->^/\/div>/' index.html
+	sed -i '11s/$<h1/<!--<h1/' index.html
+	sed -i '11s/\/h1>^/\/h1>-->/' index.html
+	sed -i "15s/src=\"[a-z0-9.:\/]*\"/src=\"$url\"/" index.html 
+else
+	if [ $1 == "off" ]; then
+		sed -i '11s/$<!--<h1/<h1/' index.html
+		sed -i '11s/\/h1>-->^/h1>/' index.html
+		sed -i '12s/$<div/<!--<div/' index.html
+		sed -i '17s/\/div>^/\/div>-->/' index.html
+	fi
+fi
+
+git commit -m "toggle that radio"
+git push origin master
